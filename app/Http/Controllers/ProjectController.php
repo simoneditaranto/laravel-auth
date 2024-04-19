6d66df21ca97c445d6ccf1e6d9155bf0e5fb5724
simoneditaranto <simodita99@gmail.com>
2024-04-19 15:20:44 +0200

    feat: CRUD: delete project aggiunto

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -82,6 +82,8 @@ class ProjectController extends Controller
      */
     public function destroy(Project $project)
     {
-        //
+        $project->delete();
+
+        return redirect()->route('admin.projects.index');
     }
 }
--- resources/views/admin/show.blade.php
+++ resources/views/admin/show.blade.php
@@ -9,7 +9,35 @@
               <h5 class="card-title">{{$project->name}}</h5>
               <p class="card-text">{{$project->description}}</p>
               <a href="{{route('admin.projects.edit', $project->id)}}" class="btn btn-warning">Modifica</a>
-              <a href="#" class="btn btn-danger">elimina</a>
+
+            <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                Elmina progetto
+            </button>
+
+            <!-- Modal -->
+            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            Sei sicuro di voler eliminare il fumetto "{{$project->name}}"?
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annulla</button>
+
+                            <form action="{{route('admin.projects.destroy', $project->id)}}" method="POST">
+                                @csrf
+                                @method("DELETE")
+
+                                <button class="btn btn-danger">Elimina</button>
+                            </form>
+
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
 
